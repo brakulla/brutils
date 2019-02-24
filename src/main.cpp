c5d3ca3fa21035a09f8cbc0bd0aeed0babcfb0fb
Burak Öngay <burakongay@gmail.com>
2019-02-24 23:45:52 +0300

    cmake file improved to include test build type

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <brutils/Signal.hpp>
 #include <thread>
 
-void testSlot(std::string input)
+void testSlot(std::string const input)
 {
     std::cout << "This is slot input parameter: " << input << std::endl;
 }
@@ -14,7 +14,7 @@ int main()
     std::cout << "Hello, World!" << std::endl;
 
     brutils::Signal<std::string> signal;
-    signal.connect(testSlot);
+    int firstConnection = signal.connect(testSlot);
     std::cout << "Connected" << std::endl;
 
     std::cout << "Doing some shit" << std::endl;
@@ -27,6 +27,11 @@ int main()
     std::cout << "Doing some more shit" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(3));
     signal.emit("Some more shit");
+
+    signal.disconnect(firstConnection);
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    signal.emit("Second shit");
 
     std::cout << "Some shit is done!" << std::endl;
 
