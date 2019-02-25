b9eb002dbf16e9c816a321e62c857f813edfaba3
burakon <BurakOngay@argela.com.tr>
2019-02-25 18:16:54 +0300

    string_utils: string manipulation utilities added

---
+++ include/brutils/string_utils.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file string_utils.h
+ *
+ * Description
+ */
+
+#ifndef BRUTILS_STRING_UTILS_H
+#define BRUTILS_STRING_UTILS_H
+
+#include <string>
+#include <sstream>
+
+namespace brutils {
+class string_utils {
+ public:
+  template<class Container>
+  static void split_string(const std::string &str, Container &cont, char delim = '') {
+    std::stringstream ss(str);
+    std::string token;
+    while (std::getline(ss, token, delim))
+      cont.push_back();
+  }
+};
+}
+
+#endif //BRUTILS_STRING_UTILS_H
