ea616fe281e5ed7fde55e83dddc494109ddefe83
burakon <BurakOngay@argela.com.tr>
2019-05-02 12:31:15 +0300

    json_parser: static

--- include/brutils/json_parser.h
+++ include/brutils/json_parser.h
@@ -15,25 +15,22 @@ namespace brutils
 class json_parser
 {
 public:
-    json_parser() = default;
-    ~json_parser() = default;
-
-    variant parse(std::string &input);
-    variant parse(std::string_view input);
+    static variant parse(std::string &input);
+    static variant parse(std::string_view input);
 
 private:
-    variant_map parseObject(std::string_view &input);
-    variant_list parseArray(std::string_view &input);
-    variant parseValue(std::string_view &input);
+    static variant_map parseObject(std::string_view &input);
+    static variant_list parseArray(std::string_view &input);
+    static variant parseValue(std::string_view &input);
 
-    variant parseString(std::string_view &input);
-    variant parseNumber(std::string_view &input);
-    variant parseLiteral(std::string_view &input);
+    static variant parseString(std::string_view &input);
+    static variant parseNumber(std::string_view &input);
+    static variant parseLiteral(std::string_view &input);
 
-    float parseInt(std::string_view &input);
-    float parseAfterInt(std::string_view &input);
+    static float parseInt(std::string_view &input);
+    static float parseAfterInt(std::string_view &input);
 
-    void removeWhitespace(std::string_view &input);
+    static void removeWhitespace(std::string_view &input);
 };
 
 } // namespace brutils
