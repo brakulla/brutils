a451c2344afaef54ea4d59519d15e7229c74b89a
congay <congay@jaguarlandrover.com>
2019-11-08 16:06:18 +0000

    RequestParser implementation continued

--- include/brutils/HttpServer/RequestParser.h
+++ include/brutils/HttpServer/RequestParser.h
@@ -22,40 +22,80 @@ enum HttpMethod {
   CONNECT,
   OPTIONS,
   TRACE,
-  UNKNOWN
+  UNKNOWN_METHOD
 };
 
 enum HttpVersion {
   HTTP_10,
   HTTP_11,
   HTTP_20,
-  HTTP_30
+  HTTP_30,
+  UNKNOWN_VERSION
+};
+
+class ParseError
+{
+
 };
 
 class RequestParser
 {
  public:
-  RequestParser();
-  ~RequestParser();
+  explicit RequestParser(br_object *parent = nullptr) :
+      newRequestReady(parent),
+      errorOccured(parent),
+      newData(parent)
+  {}
+  virtual ~RequestParser() = default;
 
- public: // signals
+  // sync api (should be called until there is no request, even if there is no new data, since there can be multiple requests in the received data)
+ public:
+  virtual std::shared_ptr<HttpRequest> newIncomingData(std::vector<uint8_t> &data) = 0;
+
+  // async api
+ public:
   signal<std::shared_ptr<HttpRequest>> newRequestReady;
+  signal<ParseError> errorOccured;
+  slot<std::vector<uint8_t>> newData;
+ protected:
+  virtual void newDataReceived_slot(std::vector<uint8_t> &data) = 0;
+
+};
+
+class RequestParser_v1x : public RequestParser
+{
+ public:
+  explicit RequestParser_v1x(br_object *parent = nullptr);
+  ~RequestParser_v1x() override;
 
  public:
-  void newIncomingData(std::vector<uint8_t> &data);
+  std::shared_ptr<HttpRequest> newIncomingData(std::vector<uint8_t> &data) override;
+
+ protected:
+  void newDataReceived_slot(std::vector<uint8_t> &data) override;
 
  private:
-  void parse();
+  // root parse function
+  bool parse();
+  // first level parse functions
   bool parseRequestLine(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end);
   bool parseHeader(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end);
   bool parseBody(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end, uint64_t contentSize);
+  // second level parse functions
+  bool parseMethod(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end);
+  bool parsePathAndQuery(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end);
+  bool parseVersion(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end);
+  // third level parse functions
+  bool parsePath(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end);
+  bool parseQuery(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end);
 
  private:
   std::vector<uint8_t > _buffer;
-  std::shared_ptr<std::string_view> _content;
   std::shared_ptr<HttpRequest> _requestInProduction;
-  std::map<std::vector<uint8_t>, HttpMethod> _methodMap;
-  std::map<std::vector<uint8_t>, HttpVersion> _versionMap;
+  const std::map<std::vector<uint8_t>, HttpMethod> _methodMap;
+  const std::map<std::vector<uint8_t>, HttpVersion> _versionMap;
+  const std::vector<uint8_t> _newLine;
+  const std::vector<uint8_t> _emptySpace;
 
   enum ParsingStatus {
     REQUEST_LINE,
--- src/brutils/HttpServer/RequestParser.cpp
+++ src/brutils/HttpServer/RequestParser.cpp
@@ -2,118 +2,266 @@
 
 using namespace brutils;
 
-RequestParser::RequestParser() :
-newRequestReady(nullptr),
-_parsingStatus(REQUEST_LINE)
-{
-  _methodMap = {
-      {{'G', 'E', 'T'}, GET},
-      {{'H', 'E', 'A', 'D'}, HEAD},
-      {{'P', 'O', 'S', 'T'}, POST},
-      {{'P', 'U', 'T'}, PUT},
-      {{'P', 'A', 'T', 'C', 'H'}, PATCH},
-      {{'D', 'E', 'L', 'E', 'T', 'E'}, DELETE},
-      {{'C', 'O', 'N', 'N', 'E', 'C', 'T'}, CONNECT},
-      {{'O', 'P', 'T', 'I', 'O', 'N', 'S'}, OPTIONS},
-      {{'T', 'R', 'A', 'C', 'E'}, TRACE}
-  };
-
-  _versionMap = {
-      {{'H', 'T', 'T', 'P', '/', '1', '.', '0'}, HTTP_10},
-      {{'H', 'T', 'T', 'P', '/', '1', '.', '1'}, HTTP_11},
-      {{'H', 'T', 'T', 'P', '/', '2', '.', '0'}, HTTP_20},
-      {{'H', 'T', 'T', 'P', '/', '3', '.', '0'}, HTTP_30}
-  };
-}
-RequestParser::~RequestParser()
+RequestParser_v1x::RequestParser_v1x(br_object *parent) :
+    RequestParser(parent),
+    _parsingStatus(REQUEST_LINE),
+    _methodMap({
+                   {{'G', 'E', 'T'}, GET},
+                   {{'H', 'E', 'A', 'D'}, HEAD},
+                   {{'P', 'O', 'S', 'T'}, POST},
+                   {{'P', 'U', 'T'}, PUT},
+                   {{'P', 'A', 'T', 'C', 'H'}, PATCH},
+                   {{'D', 'E', 'L', 'E', 'T', 'E'}, DELETE},
+                   {{'C', 'O', 'N', 'N', 'E', 'C', 'T'}, CONNECT},
+                   {{'O', 'P', 'T', 'I', 'O', 'N', 'S'}, OPTIONS},
+                   {{'T', 'R', 'A', 'C', 'E'}, TRACE}
+               }),
+    _versionMap({
+                    {{'H', 'T', 'T', 'P', '/', '1', '.', '0'}, HTTP_10},
+                    {{'H', 'T', 'T', 'P', '/', '1', '.', '1'}, HTTP_11},
+                    {{'H', 'T', 'T', 'P', '/', '2', '.', '0'}, HTTP_20},
+                    {{'H', 'T', 'T', 'P', '/', '3', '.', '0'}, HTTP_30}
+                }),
+    _newLine({{'\r', '\n'}}),
+    _emptySpace({' '})
 {
 
 }
-void RequestParser::newIncomingData(std::vector<uint8_t> &data)
+RequestParser_v1x::~RequestParser_v1x()
+{
+
+}
+std::shared_ptr<HttpRequest> RequestParser_v1x::newIncomingData(std::vector<uint8_t> &data)
 {
   _buffer.insert(_buffer.end(), data.begin(), data.end());
-  _content.reset();
-  _content = std::make_shared<std::string_view>(_buffer.cbegin(), _buffer.cend());
-  parse();
+  if (parse()) {
+    auto request = _requestInProduction;
+    _requestInProduction.reset();
+    return request;
+  }
+  return nullptr;
 }
-void RequestParser::parse()
+void RequestParser_v1x::newDataReceived_slot(std::vector<uint8_t> &data)
+{
+  _buffer.insert(_buffer.end(), data.begin(), data.end());
+  while (true) {
+    bool requestAvailable = parse();
+    if (requestAvailable) {
+      newRequestReady.emit(_requestInProduction);
+      _requestInProduction.reset();
+    } else {
+      break;
+    }
+  }
+}
+bool RequestParser_v1x::parse()
 {
   if (!_requestInProduction) {
     _requestInProduction = std::make_shared<HttpRequest>();
   }
 
-  for (auto pos = _buffer.cbegin(), end = _buffer.cend(); pos != end; ++pos) {
+  for (auto pos = _buffer.cbegin(), end = _buffer.cend(); pos != end;) {
+    // !all parsing functions used below are greedy!
     if (REQUEST_LINE == _parsingStatus) {
       if (!parseRequestLine(pos, end)) {
         _parsingStatus = ERROR;
-        break;
+      } else {
+        _parsingStatus = HEADER_LINES;
       }
-    } else if (HEADER_LINES == _parsingStatus) {
+    }
+    if (HEADER_LINES == _parsingStatus) {
       if (!parseHeader(pos, end)) {
         _parsingStatus = ERROR;
+      } else {
+//        if (_requestInProduction.header("content-size")) {
+//          _parsingStatus = BODY;
+//        } else {
+          _parsingStatus = FINISHED;
+//        }
       }
-    } else if (BODY == _parsingStatus) {
-      if (!parseBody(pos, end, 5)) { // _requestInPorduction.header("Content-Size")
-        _parsingStatus = ERROR;
-      }
-    } else if (FINISHED == _parsingStatus) {
-      newRequestReady.emit(_requestInProduction);
-    } else {
-      _parsingStatus = ERROR;
-      break;
+    }
+    if (BODY == _parsingStatus) {
+//      if (!parseBody(pos, end, _requestInProduction.header("content-size"))) {
+//        _parsingStatus = ERROR;
+//      } else {
+        _parsingStatus = FINISHED;
+//      }
+    }
+
+    if (FINISHED == _parsingStatus) {
+      _parsingStatus = REQUEST_LINE;
+//      newRequestReady.emit(_requestInProduction);
+      return true;
+    }
+    if (ERROR == _parsingStatus) {
+      errorOccured.emit(ParseError()); // send error data
+      return false;
     }
   }
+
+  return false;
 }
-bool RequestParser::parseRequestLine(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end)
+bool RequestParser_v1x::parseRequestLine(std::vector<uint8_t>::const_iterator &pos,
+                                         std::vector<uint8_t>::const_iterator &end)
 {
   if (pos == end) {
     return false;
   }
 
-
+  // get first line
+  auto lineEndPos = std::find_first_of(pos, end, _newLine.cbegin(), _newLine.cend());
 
   // read method
-  HttpMethod method = UNKNOWN;
-  auto it = pos;
-  for (; it != end && *it != ' '; ++it); // read until empty space
+  if (!parseMethod(pos, lineEndPos)) {
+    // TODO: set error
+    return false;
+  }
+
+  // read path and query
+  if (!parsePathAndQuery(pos, lineEndPos)) {
+    // TODO: set error
+    return false;
+  }
+
+  // read http protocol version
+  if (!parseVersion(pos, lineEndPos)) {
+    // TODO: set error
+    return false;
+  }
+
+  // remove new line characters
+  std::advance(pos, _newLine.size());
+  return true;
+}
+bool RequestParser_v1x::parseHeader(std::vector<uint8_t>::const_iterator &pos,
+                                    std::vector<uint8_t>::const_iterator &end)
+{
+  while (pos != end) {
+    // find end of line
+    // check length of line
+    //    if zero, finished, return
+    //    else process
+    //      if while processing it comes to the end of data, return false
+    auto lineEndPos = std::find_first_of(pos, end, _newLine.cbegin(), _newLine.end());
+    if (0 == std::distance(pos, lineEndPos)) { // processed all header lines
+      std::advance(pos, 2); // progress position by two characters to reach to the next line
+      return true;
+    }
+    auto keyEndPos = std::find(pos, lineEndPos, ':');
+    if (keyEndPos == lineEndPos) { // ':' not found
+      return false;
+    }
+    std::string key(pos, keyEndPos);
+    pos = keyEndPos;
+    std::advance(pos, 1); // advance ahead of ':'
+    std::string value(pos, lineEndPos);
+    pos = lineEndPos;
+    std::advance(pos, 2); // advance ahead of "\r\n"
+  }
+  // correct processing should be finished within while loop, so if flow reached here, it is an error
+  return false;
+}
+bool RequestParser_v1x::parseBody(std::vector<uint8_t>::const_iterator &pos,
+                                  std::vector<uint8_t>::const_iterator &end,
+                                  uint64_t contentSize)
+{
+  if (contentSize > std::distance(pos, end)) {
+    return false;
+  }
+  std::vector<uint8_t> body(pos, end);
+  pos = end;
+
+  // TODO: use body parser here or somewhere else after this point
+  return true;
+}
+bool RequestParser_v1x::parseMethod(std::vector<uint8_t>::const_iterator &pos,
+                                    std::vector<uint8_t>::const_iterator &end)
+{
+  auto methodEndPos = std::find(pos, end, ' '); // find next empty space to separate method
+  if (methodEndPos == end) { // we couldn't find empty space, something wrong
+    return false;
+  }
+  HttpMethod method = UNKNOWN_METHOD;
   for (auto &el: _methodMap) {
-    if (std::equal(pos, it, el.first.cbegin(), el.first.cend())) {
+    if (std::equal(pos, methodEndPos, el.first.cbegin(), el.first.cend())) {
       method = el.second;
       break;
     }
   }
-  if (UNKNOWN == method) {
+  if (UNKNOWN_METHOD == method) {
     return false;
   }
-  if (it == end) {
+  pos = methodEndPos;
+  std::advance(pos, 1); // advance ahead of empty space
+  // TODO: set request method
+  return true;
+}
+bool RequestParser_v1x::parsePathAndQuery(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end)
+{
+  auto uriEndPos = std::find(pos, end, ' ');
+  if (uriEndPos == end) {
     return false;
   }
-  pos = ++it; // set current position to the last read char (escaping the last empty space)
-
-  // read path (start with iterating past the first empty space till the next empty space)
-  for (; it != end && *it != ' '; ++it);
-  if (it == end) {
-    return false;
+  auto queryStartPos = std::find(pos, uriEndPos, '?');
+  if (queryStartPos == uriEndPos) {
+    parsePath(pos, uriEndPos);
+  } else {
+    if (!parsePath(pos, queryStartPos)) {
+      return false;
+    }
+    std::advance(queryStartPos, 1); // advance ahead of '?' char
+    if (!parseQuery(pos, uriEndPos)) {
+      return false;
+    }
   }
-  std::string path(pos, it);
-  pos = ++it; // set current position to the last read char (escaping the last empty space)
-
-  // read http version
-  HttpVersion  version;
-  for (; it != end && *it != '\n'; ++it); // read until the end of request line
+  return true;
+}
+bool RequestParser_v1x::parseVersion(std::vector<uint8_t>::const_iterator &pos,
+                                     std::vector<uint8_t>::const_iterator &end)
+{
+  // read http version, this time, we read until the end of the line
+  HttpVersion version = UNKNOWN_VERSION;
   for (auto &el: _versionMap) {
-    if (std::equal(pos, it, el.first.cbegin(), el.first.cend()) {
+    if (std::equal(pos, end, el.first.cbegin(), el.first.cend())) {
       version = el.second;
       break;
     }
   }
+  if (UNKNOWN_VERSION == version) {
+    return false;
+  }
+  pos = end;
 
+  return version;
 }
-bool RequestParser::parseHeader(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end)
+bool RequestParser_v1x::parsePath(std::vector<uint8_t>::const_iterator &pos,
+                                         std::vector<uint8_t>::const_iterator &end)
 {
-
+  if (0 >= std::distance(pos, end)) {
+    return false;
+  }
+  std::string path(pos, end);
+  // TODO: set request path
+  pos = end;
+  return true;
 }
-bool RequestParser::parseBody(std::vector<uint8_t>::const_iterator &pos, std::vector<uint8_t>::const_iterator &end, uint64_t contentSize)
+bool RequestParser_v1x::parseQuery(std::vector<uint8_t>::const_iterator &pos,
+                                                                 std::vector<uint8_t>::const_iterator &end)
 {
-
+  std::map<std::string, std::string> query;
+  while(0 < std::distance(pos, end)) {
+    auto pairEndPos = std::find(pos, end, '&');
+    auto keyEndPos = std::find(pos, pairEndPos, '=');
+    if (keyEndPos == pairEndPos) {
+      query.emplace(std::string(pos, pairEndPos), "");
+    } else {
+      auto valueStartPos = keyEndPos;
+      std::advance(valueStartPos, 1);
+      query.emplace(std::string(pos, keyEndPos), std::string(valueStartPos, pairEndPos));
+    }
+    pos = pairEndPos;
+    std::advance(pos, 1); // advance ahead of '&' or '='
+  }
+  // TODO; set request query
+  return true;
 }
